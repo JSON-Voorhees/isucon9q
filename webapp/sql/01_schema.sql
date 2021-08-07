b69b67f9cfc46ec643dfbb52f2dbd2504311d26d
sh-lu0 <chiaki.m.515@gmail.com>
2021-08-07 13:07:00 +0900

    itemsテーブルにcreated_atとidのINDEX追加

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE `items` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_id (`id`),
   INDEX idx_category_id (`category_id`),
-  INDEX idx_created_at_id (`created_at`, `created_at`)
+  INDEX idx_created_at_id (`created_at`, `id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
